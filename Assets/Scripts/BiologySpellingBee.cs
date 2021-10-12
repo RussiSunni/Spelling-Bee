05fa236559ae5f6a91df358cfbb2ed3b97a72815
Jonathan Dyason <jonathandyason@hotmail.com>
2021-10-12 11:02:46 +0200

    asd

--- Assets/Scripts/BiologySpellingBee.cs
+++ Assets/Scripts/BiologySpellingBee.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BiologySpellingBee : MonoBehaviour
 {
+    public Text evaluationText;
+    public InputField inputField;
     void Start()
     {
         GameObject soundManager = GameObject.Find("SoundManager");
         SoundManager soundManagerScript = soundManager.GetComponent<SoundManager>();
+
+
         soundManagerScript.playQuestionSound(BiologyQuestionBank.questions[0].questionAudio);
+    }
+
+    public void CheckButton()
+    {
+        if (inputField.text == "atom")
+        {
+            evaluationText.text = "correct";
+        }
+        else
+        {
+            evaluationText.text = "incorrect";
+        }
     }
 }
